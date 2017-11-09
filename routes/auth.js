ccf63de73c51283820ae3ed191c96b1d6b185109
Camilo Segura <camilosegurar@gmail.com>
2017-11-08 22:50:04 -0500

    feat: added google auth and callback routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,15 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/google', 
-    passport.authenticate('google', {scope: ['profile', 'email']}), (req, res, next) => {
-//   res.render('auth', { title: 'Express' });
+    passport.authenticate('google', {scope: ['profile', 'email']}), 
+    (req, res, next) => {
+});
+
+router.get('/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+  // Successful authentication, redirect home.
+        res.redirect('/dashboard');
 });
 
 module.exports = router;
